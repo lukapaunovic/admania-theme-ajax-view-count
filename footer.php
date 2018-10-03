9c29d885063b4efc1a87394766abc0cfc55b50e3
Luka Paunović <office@leo-host.com>
2018-10-04 01:17:06 +0200

    Create footer.php
    Place this before the </body> tag in /wp-content/themes/admania/footer.php

---
+++ footer.php
@@ -0,0 +1,18 @@
+<script>
+jQuery(document).ready(function( $ ) {
+	$.ajax({
+		type: 'POST',
+		url: '/wp-content/themes/admania/ajax-view-count.php',
+		data: {
+			post_id: <?php echo get_the_ID(); ?>
+		},
+		success: function(data) {
+			$('.admania_viewcnt').html(" " + data.views_count + " <span class='admania_view_text'> Views </span>");
+			console.log("View count: " + data.views_count);
+		},
+		error: function() {
+			console.log("Something is wrong with Ajax View Count");
+		}
+	});
+});
+</script>
